40435c22f94c4bf128df8aa8359998ff4103475b
Simon Prophet <simon.s.prophet@outlook.com>
2023-05-12 13:03:55 +0100

    add page split function and more css styles

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', function () {
+  container.classList.add('hover-left');
+  leftOverlay.style.width = 0;
+  leftOverlay.style.height = 0;
+  leftOverlay.style.backgroundColor = 'transparent';
+});
+
+left.addEventListener('mouseleave', () =>
+  container.classList.remove('hover-left')
+);
+
+right.addEventListener('mouseenter', () =>
+  container.classList.add('hover-right')
+);
+right.addEventListener('mouseleave', () =>
+  container.classList.remove('hover-right')
+);
